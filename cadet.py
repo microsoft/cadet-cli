1a7527e0f70aa85f5b00b467c46e8b19858f3eef
Hannah Kennedy <hannah.kennedy@microsoft.com>
2019-01-08 18:00:10 -0500

    Adds a number of try-catch statements for better error-handling.

--- cadet.py
+++ cadet.py
@@ -9,6 +9,9 @@ import azure.cosmos.cosmos_client as cosmos_client
 
 @click.group()
 def main():
+    """
+    Acts as a super-group for the import command and any future commands to cadet
+    """
     pass
 
 
@@ -27,7 +30,7 @@ def main():
 )
 @click.option(
     '--database-name', '-d',
-    help="The database name to connect to"
+    help="The name of the database to connect to"
     )
 @click.option(
     '--collection-name', '-c',
@@ -40,64 +43,72 @@ def main():
 @click.argument('source')
 def upload(source, type_, collection_name, database_name, endpoint, primary_key, uri):
     """
-    Assumes that the Cosmos DB subscription has both the database and the collection already 
+    Assumes that the Cosmos DB subscription has both the database and the collection already
     made when running this tool.
-
     """
     # Make sure it's a CSV or TSV
     type_ = type_.upper()
     if type_ not in ["CSV", "TSV"]:
         raise click.BadParameter("We currently only support CSV and TSV uploads from Cadet")
-        return
 
     delimiters = {
         "CSV": ',',
         "TSV": '\t'
     }
 
-    # you must have either the connection string OR (endpoint and key) to connect
-    if (endpoint is None or primary_key is none) and (uri is None):
-        raise click.BadParameter("You must either have a connection string OR *both* an endpoint and a primary key in order to use Cadet")
-        return
+    # You must have either the connection string OR (endpoint and key) to connect
+    if (endpoint is None or primary_key is None) and (uri is None):
+        raise click.BadParameter(
+            "You must have a connection string OR *both* an endpoint and a key to use Cadet"
+            )
     elif endpoint is not None and primary_key is not None:
         _connection_url = endpoint
         _auth = {'masterKey': primary_key}
-    else:
-        #if someone provides the connection string, break it apart into its subcomponents
-        uri_array = uri.split(';')
-        _connection_url = uri_array[0].replace('AccountEndpoint=','')
-        _auth = {'masterKey': uri_array[1].replace('AccountKey','')}
+    elif uri is not None:
+        # If someone provides the connection string, break it apart into its subcomponents
+        try:
+            uri_array = uri.split(';')
+            _connection_url = uri_array[0].replace('AccountEndpoint=', '')
+            _auth = {'masterKey': uri_array[1].replace('AccountKey', '')}
+        except: # NOT HIT?
+            raise click.BadParameter("The connection string is not properly formatted - aborting")
 
 
     database_link = 'dbs/' + database_name
     collection_link = database_link + '/colls/' + collection_name
 
-    client = cosmos_client.CosmosClient(
-        url_connection=_connection_url,
-        auth=_auth
-        )
+    try:
+        client = cosmos_client.CosmosClient(
+            url_connection=_connection_url,
+            auth=_auth
+            )
+    except:
+        raise click.BadParameter("Authentication failure to Azure Cosmos")
 
-    #stats read for percentage done
+    # Stats read for percentage done
     source_size = os.stat(source).st_size
     print('Source file total size is:', source_size, 'bytes\n')
 
-    #read and upload at same time
-    print('Starting the upload')
-    with open(source, 'r') as source: 
-        document = {}
-        csv_reader = csv.reader(source, delimiter=delimiters[type_])
-        line_count = 0
-        with click.progressbar(length=source_size, show_percent=True) as bar:
-            for row in csv_reader:
-                if line_count == 0:
-                    csv_cols = row
-                    line_count += 1
-                else:
-                    for ind, col in enumerate(csv_cols):
-                        document[csv_cols[ind]] = row[ind]
-                    result = client.UpsertItem(collection_link, document)
-                    bar.update(sys.getsizeof(document))
-    print('Upload complete!')
+    # Read and upload at same time
+    try:
+        with open(source, 'r') as source_file:
+            print('Starting the upload')
+            document = {}
+            csv_reader = csv.reader(source_file, delimiter=delimiters[type_])
+            line_count = 0
+            with click.progressbar(length=source_size, show_percent=True) as status_bar:
+                for row in csv_reader:
+                    if line_count == 0:
+                        csv_cols = row
+                        line_count += 1
+                    else:
+                        for ind, col in enumerate(csv_cols):
+                            document[csv_cols[ind]] = row[ind]
+                        result = client.UpsertItem(collection_link, document)
+                        status_bar.update(sys.getsizeof(document))
+        print('Upload complete!')
+    except:
+        raise click.FileError(source)
 
 if __name__ == "__main__":
     main()
