5fd8531930e92534d10e3b133bf4d8289b2ed6b6
AndrewFrancisque <anfranc@microsoft.com>
2019-01-23 16:34:39 -0500

    Add docstrings, remove instances of 'self' on classes

--- src/test/test_cadet.py
+++ src/test/test_cadet.py
@@ -1,3 +1,7 @@
+"""
+Imports module necessary for testing
+"""
+
 import copy
 from unittest import mock
 
@@ -26,23 +30,42 @@ TEST_DB = 'TestDB'
 TEST_COLLECTION = 'TestCollection'
 RUNNER = CliRunner()
 
-class MockClient(object):
+class MockClient:
+    """
+    Class constructed to mock Azure CosmosClient connection and functions
+    """
+
     def __init__(self):
         self.upserted_docs = list()
 
-    def UpsertItem(self, collectionLink, document):
+    def UpsertItem(self, _collection_link, document):
+        """
+        Mocks Azure Client's UpsertItem function
+        """
+
         self.upserted_docs.append(copy.copy(document))
 
     def CosmosClient(self, url_connection, auth):
+        """
+        Mocks Azure CosmosClient's authentication and connection process
+        """
+
         raise ConnectionError('Authentication failure to Azure Cosmos')
 
-class TestClass(object):
-    # Tests that, given all required options, including a primary Key and URI combo
-    # and a CSV file, the tool works as expected
+class TestClass:
+    """
+    Class constructed to hold tests
+    """
+
     @mock.patch('src.cadet.get_cosmos_client', autospec=True)
     def test_all_good_params_uri_primary_key_csv(self, mock_get_cosmos_client):
-        MC = MockClient()
-        mock_get_cosmos_client.return_value = MC
+        """
+        Tests that, given all required options, including a primary Key and URI combo
+        and a CSV file, the tool works as expected
+        """
+
+        mock_client = MockClient()
+        mock_get_cosmos_client.return_value = mock_client
         result = RUNNER.invoke(
             upload, [GOOD_CSV, '--type', CSV_TYPE, '-d', TEST_DB,
                      '-c', TEST_COLLECTION, '-u', TEST_URI, '-k', TEST_KEY]
@@ -60,8 +83,8 @@ class TestClass(object):
         expected_values.append(['0', '172534', 'CLAY COUNTY', 'FL'])
 
         # Collection assert that expected values and actual upserted values are equal
-        for num in range(len(MC.upserted_docs)):
-            vals = MC.upserted_docs[num]
+        for num in range(len(mock_client.upserted_docs)):
+            vals = mock_client.upserted_docs[num]
 
             keys = list(vals.keys())
             vals = list(vals.values())
@@ -71,15 +94,18 @@ class TestClass(object):
             assert keys == expected_keys
             assert vals == expected_values[num]
 
-        assert len(MC.upserted_docs) == 5
+        assert len(mock_client.upserted_docs) == 5
         assert result.exit_code == 0
 
-    # Tests that, given all required options, using a connection string and a CSV file,
-    # the tool works as expected
     @mock.patch('src.cadet.get_cosmos_client', autospec=True)
     def test_all_good_params_connection_string_csv(self, mock_get_cosmos_client):
-        MC = MockClient()
-        mock_get_cosmos_client.return_value = MC
+        """
+        Tests that, given all required options, using a connection string and a CSV file,
+        the tool works as expected
+        """
+
+        mock_client = MockClient()
+        mock_get_cosmos_client.return_value = mock_client
         result = RUNNER.invoke(
             upload, [GOOD_CSV, '--type', CSV_TYPE, '--database-name', TEST_DB,
                      '--collection-name', TEST_COLLECTION, '--connection-string', TEST_CONN_STRING]
@@ -98,8 +124,8 @@ class TestClass(object):
         expected_values.append(['0', '172534', 'CLAY COUNTY', 'FL'])
 
         # Collection assert that expected values and actual upserted values are equal
-        for num in range(len(MC.upserted_docs)):
-            vals = MC.upserted_docs[num]
+        for num in range(len(mock_client.upserted_docs)):
+            vals = mock_client.upserted_docs[num]
 
             keys = list(vals.keys())
             vals = list(vals.values())
@@ -109,16 +135,19 @@ class TestClass(object):
             assert keys == expected_keys
             assert vals == expected_values[num]
 
-        assert len(MC.upserted_docs) == 5
+        assert len(mock_client.upserted_docs) == 5
         assert result.exit_code == 0
 
 
-    # Tests that, given all required options, including a primary Key and URI combo
-    # and a TSV file, the tool works as expected
     @mock.patch('src.cadet.get_cosmos_client', autospec=True)
     def test_all_good_params_uri_primary_key_tsv(self, mock_get_cosmos_client):
-        MC = MockClient()
-        mock_get_cosmos_client.return_value = MC
+        """
+        Tests that, given all required options, including a primary Key and URI combo
+        and a TSV file, the tool works as expected
+        """
+
+        mock_client = MockClient()
+        mock_get_cosmos_client.return_value = mock_client
         result = RUNNER.invoke(
             upload, [GOOD_TSV, '--type', TSV_TYPE, '-d', TEST_DB,
                      '-c', TEST_COLLECTION, '-u', TEST_URI, '-k', TEST_KEY]
@@ -134,8 +163,8 @@ class TestClass(object):
         expected_values.append(['45', 'W Main St', 'Zeke'])
 
         # Collection assert that expected values and actual upserted values are equal
-        for num in range(len(MC.upserted_docs)):
-            vals = MC.upserted_docs[num]
+        for num in range(len(mock_client.upserted_docs)):
+            vals = mock_client.upserted_docs[num]
 
             keys = list(vals.keys())
             vals = list(vals.values())
@@ -145,15 +174,18 @@ class TestClass(object):
             assert keys == expected_keys
             assert vals == expected_values[num]
 
-        assert len(MC.upserted_docs) == 3
+        assert len(mock_client.upserted_docs) == 3
         assert result.exit_code == 0
 
-    # Tests that, given all required options, using a connection string and a TSV file,
-    # the tool works as expected
     @mock.patch('src.cadet.get_cosmos_client', autospec=True)
     def test_all_good_params_connection_string_tsv(self, mock_get_cosmos_client):
-        MC = MockClient()
-        mock_get_cosmos_client.return_value = MC
+        """
+        Tests that, given all required options, using a connection string and a TSV file,
+        the tool works as expected
+        """
+
+        mock_client = MockClient()
+        mock_get_cosmos_client.return_value = mock_client
         result = RUNNER.invoke(
             upload, [GOOD_TSV, '--type', TSV_TYPE, '--database-name', TEST_DB,
                      '--collection-name', TEST_COLLECTION, '--connection-string', TEST_CONN_STRING]
@@ -170,8 +202,8 @@ class TestClass(object):
         expected_values.append(['45', 'W Main St', 'Zeke'])
 
         # Collection assert that expected values and actual upserted values are equal
-        for num in range(len(MC.upserted_docs)):
-            vals = MC.upserted_docs[num]
+        for num in range(len(mock_client.upserted_docs)):
+            vals = mock_client.upserted_docs[num]
 
             keys = list(vals.keys())
             vals = list(vals.values())
@@ -181,11 +213,13 @@ class TestClass(object):
             assert keys == expected_keys
             assert vals == expected_values[num]
 
-        assert len(MC.upserted_docs) == 3
+        assert len(mock_client.upserted_docs) == 3
         assert result.exit_code == 0
 
-    # Tests that source file needs to be present
     def test_source_is_missing(self):
+        """
+        Tests that source file needs to be present
+        """
         result = RUNNER.invoke(
             upload, ['--type', CSV_TYPE, '--database-name', TEST_DB,
                      '--collection-name', TEST_COLLECTION, '-u', TEST_URI, '-k', TEST_KEY]
@@ -193,8 +227,11 @@ class TestClass(object):
         assert result.exit_code != 0
         assert 'Missing argument "SOURCE"' in result.output
 
-    # Tests that source file extension needs to be .csv or .tsv
     def test_source_file_txt(self):
+        """
+        Tests that source file extension needs to be .csv or .tsv
+        """
+
         result = RUNNER.invoke(
             upload, [TXT_TEST_FILE, '--type', CSV_TYPE, '--database-name', TEST_DB,
                      '--collection-name', TEST_COLLECTION, '-u', TEST_URI, '-k', TEST_KEY]
@@ -202,8 +239,11 @@ class TestClass(object):
         assert result.exit_code != 0
         assert 'We currently only support CSV and TSV uploads from Cadet' in result.output
 
-    # Tests that DB name needs to be present
     def test_db_not_present(self):
+        """
+        Tests that DB name needs to be present
+        """
+
         result = RUNNER.invoke(
             upload, [GOOD_CSV, '--type', CSV_TYPE, '--collection-name', TEST_COLLECTION,
                      '-u', TEST_URI, '-k', TEST_KEY]
@@ -211,8 +251,11 @@ class TestClass(object):
         assert result.exit_code != 0
         assert 'Missing option "--database-name"' in result.output
 
-    # Tests that collection name needs to be present
     def test_collection_not_present(self):
+        """
+        Tests that collection name needs to be present
+        """
+
         result = RUNNER.invoke(
             upload, [GOOD_CSV, '--type', CSV_TYPE, '--database-name',
                      TEST_DB, '-u', TEST_URI, '-k', TEST_KEY]
@@ -220,8 +263,11 @@ class TestClass(object):
         assert result.exit_code != 0
         assert 'Missing option "--collection-name"' in result.output
 
-    # Tests that connection string or primary key/URI needs to be present
     def test_connection_string_uri_and_primary_key_absence(self):
+        """
+        Tests that connection string or primary key/URI needs to be present
+        """
+
         result = RUNNER.invoke(
             upload, [GOOD_CSV, '--type', CSV_TYPE, '--database-name', TEST_DB,
                      '--collection-name', TEST_COLLECTION]
@@ -229,8 +275,11 @@ class TestClass(object):
         assert result.exit_code != 0
         assert 'REQUIRED: Connection string OR *both* a URI and a key' in result.output
 
-    # Tests that URI must be present, if connection string not present
     def test_uri_absent_key_present(self):
+        """
+        Tests that URI must be present, if connection string not present
+        """
+
         result = RUNNER.invoke(
             upload, [GOOD_CSV, '--type', CSV_TYPE, '--database-name', TEST_DB,
                      '--collection-name', TEST_COLLECTION, '-k', TEST_KEY]
@@ -238,8 +287,11 @@ class TestClass(object):
         assert result.exit_code != 0
         assert 'REQUIRED: Connection string OR *both* a URI and a key' in result.output
 
-    # Tests that primary key must be present if connection string not present
     def test_primary_key_absent_uri_present(self):
+        """
+        Tests that primary key must be present if connection string not present
+        """
+
         result = RUNNER.invoke(
             upload, [GOOD_CSV, '--type', CSV_TYPE, '--database-name', TEST_DB,
                      '--collection-name', TEST_COLLECTION, '-u', TEST_URI]
@@ -247,8 +299,11 @@ class TestClass(object):
         assert result.exit_code != 0
         assert 'REQUIRED: Connection string OR *both* a URI and a key' in result.output
 
-    # Tests that connection string is correctly parsed
     def test_connection_string_parsing(self):
+        """
+        Tests that connection string is correctly parsed
+        """
+
         invalid_uri = 'AccountEndpoint=invalidEndpoint'
         invalid_account_key = 'AccountKey:invalidKey;'
         invalid_connection_string = invalid_uri + invalid_account_key
@@ -259,11 +314,14 @@ class TestClass(object):
         assert result.exit_code != 0
         assert 'The connection string is not properly formatted - aborting' in result.output
 
-    # Test that cosmos_client error throwing functionality, if connection to service fails
     @mock.patch('src.cadet.cosmos_client', autospec=True)
     def test_cosmos_client_throws_error(self, mock_cosmos_client):
-        MC = MockClient()
-        mock_cosmos_client.CosmosClient.side_effect = MC.CosmosClient
+        """
+        Test that cosmos_client error throwing functionality, if connection to service fails
+        """
+
+        mock_client = MockClient()
+        mock_cosmos_client.CosmosClient.side_effect = mock_client.CosmosClient
         result = RUNNER.invoke(
             upload, [GOOD_TSV, '--type', TSV_TYPE, '--database-name', TEST_DB,
                      '--collection-name', TEST_COLLECTION, '--conn-string', TEST_CONN_STRING]
